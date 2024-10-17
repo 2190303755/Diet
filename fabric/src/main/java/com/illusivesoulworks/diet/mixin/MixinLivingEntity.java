b579012cee96e1afe96f4d6ba147efacbc7a86f6
2190303755 <2190303755@qq.com>
2024-10-17 11:54:11 +0800

    compat

--- fabric/src/main/java/com/illusivesoulworks/diet/mixin/MixinLivingEntity.java
+++ fabric/src/main/java/com/illusivesoulworks/diet/mixin/MixinLivingEntity.java
@@ -18,50 +18,28 @@
 package com.illusivesoulworks.diet.mixin;
 
 import com.illusivesoulworks.diet.common.component.DietComponents;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class MixinLivingEntity {
-
-  @Shadow
-  protected ItemStack useItem;
-
-  @Unique
-  private ItemStack diet$copy = ItemStack.EMPTY;
-
   @Inject(
-      at = @At(
-          value = "INVOKE",
-          target = "net/minecraft/world/item/ItemStack.finishUsingItem(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;"),
-      method = "completeUsingItem()V")
-  private void diet$completeUsingItem1(CallbackInfo cb) {
-    this.diet$copy = this.useItem.copy();
-  }
-
-  @Inject(
-      at = @At(
-          value = "INVOKE_ASSIGN",
-          target = "net/minecraft/world/item/ItemStack.finishUsingItem(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;"),
-      method = "completeUsingItem()V")
-  private void diet$completeUsingItem2(CallbackInfo cb) {
-
-    if (!((LivingEntity) (Object) this).level().isClientSide() &&
-        ((LivingEntity) (Object) this) instanceof Player) {
-      FoodProperties food = diet$copy.getItem().getFoodProperties();
-
-      if (food != null) {
-        DietComponents.DIET_TRACKER.maybeGet(this).ifPresent(diet -> diet.consume(diet$copy));
-      }
+          method = "eat",
+          at = @At(
+                  value = "INVOKE",
+                  target = "Lnet/minecraft/world/entity/LivingEntity;gameEvent(Lnet/minecraft/world/level/gameevent/GameEvent;)V"
+          )
+  )
+  private void diet$onEat(Level level, ItemStack stack, CallbackInfoReturnable<ItemStack> info) {
+    //noinspection ConstantValue
+    if ((Object) this instanceof ServerPlayer && stack.getItem().getFoodProperties() != null) {
+      DietComponents.DIET_TRACKER.maybeGet(this).ifPresent(diet -> diet.consume(stack));
     }
-    diet$copy = ItemStack.EMPTY;
   }
 }
